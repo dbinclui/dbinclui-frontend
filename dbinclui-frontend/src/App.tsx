170d1151576a445ae1b06d2d552c56faa81b98a9
Nicole Siqueira Fagundes <psi.fagundes@gmail.com>
2021-12-13 19:30:09 -0300

    configuração ambiente front

---
+++ dbinclui-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import VLibras from '@djpfs/react-vlibras';
+
+function App() {
+  return (
+    <div className="App">
+      <VLibras />
+      <h1>Teste</h1>
+     
+    </div>
+  );
+}
+
+export default App;
