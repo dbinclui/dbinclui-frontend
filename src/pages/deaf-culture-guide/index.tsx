76bcf88ee4570dabe1e400d86284cb59022e0ba0
Lorena da Silva Azevedo <lorenaazevedo41@gmail.com>
2021-12-16 17:38:31 -0300

    alteração da versão do jest e configuração das rotas e páginas (home, guias de acessibilidade e de erro)

---
+++ src/pages/deaf-culture-guide/index.tsx
@@ -0,0 +1,5 @@
+export default function DeafCultureGuide() {
+    return (
+        <p>Teste página guia cultura surda</p>
+    )
+}
---
+++ src/pages/error/index.tsx
@@ -0,0 +1,5 @@
+export default function Error() {
+    return (
+        <p>Página não encontrada</p>
+    )
+}
