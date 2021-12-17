9ab519ecabcdce5296224b7f4d2df0edc2362243
Dayane Mendes <day.neisca@gmail.com>
2021-12-17 14:04:55 -0300

    Adicionando pag 404

---
+++ src/pages/error/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function Error() {
+  return (
+    <>
+      <div>
+        <p>Desculpe, a página não foi encontrada!</p>
+      </div>
+    </>
+  );
+}
