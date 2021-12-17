5f4a2fef121b07acd8c7bc6caccfa412e9897b27
Bruno Machado <brunomdr46@gmail.com>
2021-12-17 17:28:21 -0300

    Criação de testes unitários

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Card from "./index";
+import AcessibilityGuide from "../../pages/acessibility-guide/index";
+import CardAcessibilidade from "../../components/Card/index";
 
 describe("Componente do Card", () => {
   test("deve mostrar um Card com texto", () => {
@@ -8,4 +10,30 @@ describe("Componente do Card", () => {
     const cardTitle = screen.getByText("O que é acessibilidade?");
     expect(cardTitle).toBeInTheDocument();
   });
+
+  test("deve mostrar a página do Guia de Acessibilidade", () => {
+    render(<AcessibilityGuide />);
+
+    const pageTitle = screen.getByText("Guia de Acessibilidade");
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+  test("deve mostrar um Card com texto", () => {
+    expect(CardAcessibilidade).toBeTruthy();
+  });
 });
+
+// describe("Página Guia de Acessibilidade", () => {
+//   test("deve mostrar a página do Guia de Acessibilidade", () => {
+//     render(<AcessibilityGuide />);
+
+//     const pageTitle = screen.getByText("Guia de Acessibilidade");
+//     expect(pageTitle).toBeInTheDocument();
+//   });
+// });
+
+// describe("Função dos cards do Guia de Acessibilidade", () => {
+//   test("deve mostrar um Card com texto", () => {
+//     expect(CardAcessibilidade).toBeTruthy();
+//   });
+// });
