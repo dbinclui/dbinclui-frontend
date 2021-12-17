6c065107f5f406c3a93e150c1d934c2cc69976d6
Dayane Mendes <day.neisca@gmail.com>
2021-12-17 17:05:48 -0300

    Alterações no index

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../src/pages/home';
-import App from './App';
-import AcessibilityGuide from './pages/acessibility-guide';
-import DeafCultureGuide from './pages/deaf-culture-guide';
-import Error from './pages/error/404';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../src/pages/home";
+import App from "./App";
+import AcessibilityGuide from "./pages/acessibility-guide";
+import DeafCultureGuide from "./pages/deaf-culture-guide";
+import Error from "./pages/error/404";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>}/>
-          <Route index element={<Home/>}/>
-            <Route path="guia-acessibilidade" element={<AcessibilityGuide/>}/>
-            <Route path="guia-cultura-surda" element={<DeafCultureGuide/>}/>
-          <Route path="*" element={<Error/>}/>
-
+        <Route path="/" element={<App />} />
+        <Route index element={<Home />} />
+        <Route path="guia-acessibilidade" element={<AcessibilityGuide />} />
+        <Route path="guia-cultura-surda" element={<DeafCultureGuide />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
