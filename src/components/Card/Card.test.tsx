86a013615c8923770a30be04f58d1e440e48754e
Bruno Machado <brunomdr46@gmail.com>
2021-12-20 11:41:53 -0300

    Alteração nos testes

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -3,15 +3,16 @@ import Card from "./index";
 import CardAcessibilidade from "../../components/Card/index";
 
 describe("Componente do Card", () => {
-  test("deve mostrar um Card com texto", () => {
-    render(<Card title={""} />);
-
-    const cardTitle = screen.getByText("O que é acessibilidade?");
-    expect(cardTitle).toBeInTheDocument();
+  test("deve existir um Card na página", () => {
+    expect(CardAcessibilidade).toBeTruthy();
   });
 
   test("deve mostrar um Card com texto", () => {
-    expect(CardAcessibilidade).toBeTruthy();
+    render(<Card title={"O que é acessibilidade?"} />);
+
+    const pageTitleContent = 'O que é acessibilidade?';
+    const pageTitle = screen.getByText(pageTitleContent);
+    expect(pageTitle.textContent).toBe(pageTitleContent);
   });
 });
 
--- src/pages/acessibility-guide/AcessibilityGuide.test.tsx
+++ src/pages/acessibility-guide/AcessibilityGuide.test.tsx
@@ -3,7 +3,7 @@ import AcessibilityGuide from '../../pages/acessibility-guide/index';
 import CardAcessibilidade from '../../components/Card/index';
 
 describe('Página do Guia de Acessibilidade', () => {
-  test('deve mostrar a página do Guia de Acessibilidade', () => {
+  test('deve mostrar o título da página do Guia de Acessibilidade', () => {
     render(<AcessibilityGuide />);
 
     const pageTitleContent = 'Guia de Acessibilidade';
