4c41ff70194118b886227a741e18c8732075e413
Lorena da Silva Azevedo <lorenaazevedo41@gmail.com>
2021-12-21 11:15:45 -0300

    insere estrutura de rotas no app.tsx e importa somente app no index

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../src/pages/home';
 import App from './App';
-import AcessibilityGuide from './pages/acessibility-guide';
-import DeafCultureGuide from './pages/deaf-culture-guide';
-import Error from './pages/error/404';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="guia-acessibilidade" element={<AcessibilityGuide />} />
-          <Route path="guia-cultura-surda" element={<DeafCultureGuide />} />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root'),
 );
