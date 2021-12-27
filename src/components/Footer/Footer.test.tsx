7c27c085a4fea9610d9f0bb92f875b843d1dd681
Bruno Machado <brunomdr46@gmail.com>
2021-12-27 17:14:44 -0300

    Alterações no teste do componente

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import Footer from './index';
 
 describe('Componente do Footer', () => {
   test('deve existir um footer na página', () => {
     expect(Footer).toBeTruthy();
   });
-
+});
