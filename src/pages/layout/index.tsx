43033521ff05795f2553a2fd3051ea91ba37bc39
nickolasL13 <nickk.lima17@gmail.com>
2021-12-28 17:51:57 -0300

    Atualização de footer

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../components/Footer';
 export interface LayoutProps {}
 
 export const Layout: React.FC<LayoutProps> = (): JSX.Element => {
-  return <>
-  <Outlet />
-  </>;
+  return (
+    <>
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
