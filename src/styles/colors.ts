6d08c70c4a6e217d84d4528aded9352866521ac6
LizSantana <liz.santanabrito@gmail.com>
2021-12-28 10:50:59 -0300

    style: criaçao do global.css e colors.ts

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+//eslint-disable-next-line no-shadow
+export enum Colors {
+  WHITE = '#FFF',
+  BLACK = '#000',
+  DARK_BLUE = '#221F52',
+  PINK = '#ED0973',
+  RED = '#FF1229',
+  YELLOW = '#FFB400',
+  GREEN = '#42CE95',
+  LIGHT_BLUE = '#14B6EF'
+}
