9c7d19be7af03a877e8511a22cd6f5dbb797e0fd
Bruno Machado <brunomdr46@gmail.com>
2021-12-28 15:23:04 -0300

    Tentando resolver erro de build

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,14 +4,14 @@ import './styles.css';
 export default function Home() {
   return (
     <>
-    <div className="inputPesquisa">
-      <input
-        type="search"
-        aria-label="Pesquisar conteúdo no site"
-        placeholder="Pesquisar..."
-        autoComplete="off"
-      ></input>
-    </div>
+      <div className="inputPesquisa">
+        <input
+          type="search"
+          aria-label="Pesquisar conteúdo no site"
+          placeholder="Pesquisar..."
+          autoComplete="off"
+        ></input>
+      </div>
       <p>Bem-vindo ao DBInclui, </p>
     </>
   );
