103bfe66c96a28c669d95d93024452e819a5585a
Ketryn <ketryn29@gmail.com>
2021-12-28 16:47:13 -0300

    teste-mui

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
 
 import './style.css';
 
@@ -74,8 +81,36 @@ export const Home: React.FC<HomeProps> = (): JSX.Element => {
             <img src="" alt="" />
           </div>
         </div>
+        <Button variant="contained"> Olá Mundo</Button>;
+        <FormControl component="fieldset">
+      <FormLabel component="legend">labelPlacement</FormLabel>
+      <RadioGroup row aria-label="position" name="position" defaultValue="top">
+        <FormControlLabel
+          value="top"
+          control={<Radio />}
+          label="Top"
+          labelPlacement="top"
+        />
+        <FormControlLabel
+          value="start"
+          control={<Radio />}
+          label="Start"
+          labelPlacement="start"
+        />
+        <FormControlLabel
+          value="bottom"
+          control={<Radio />}
+          label="Bottom"
+          labelPlacement="bottom"
+        />
+        <FormControlLabel value="end" control={<Radio />} label="End" />
+      </RadioGroup>
+    </FormControl>
+        
+
       </section>
     </>
+
   );
 };
 
