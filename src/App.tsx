26510cb0adeb833daf9fc80c0b69c42eb90d0a30
nickolasL13 <nickk.lima17@gmail.com>
2021-12-28 17:54:13 -0300

    Ajuste Yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import VLibras from '@djpfs/react-vlibras';
 import Routers from './routes/routes';
-import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
