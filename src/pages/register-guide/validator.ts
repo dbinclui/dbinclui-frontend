753b4b697f07aa898a85e4a95f45f848b442025c
arthurvergacas <arthurvdm@gmail.com>
2021-12-29 18:18:57 -0300

    [chore] Remove console.log and fix formatting

--- src/pages/register-guide/validator.ts
+++ src/pages/register-guide/validator.ts
@@ -1,22 +1,26 @@
 import * as yup from 'yup';
 
-export default async function validateInput(data: { title: string; description: string }) {
-    const schema = yup.object().shape({
-      title: yup.string().required('O título é obrigatório').min(1).max(32, 'O título é muito grande'),
-      description: yup.string().required('A descrição é obrigatória'),
-    });
+export default async function validateInput(data: {
+  title: string;
+  description: string;
+}) {
+  const schema = yup.object().shape({
+    title: yup
+      .string()
+      .required('O título é obrigatório')
+      .min(1)
+      .max(32, 'O título é muito grande'),
+    description: yup.string().required('A descrição é obrigatória'),
+  });
 
-    try {
-        let valid = await schema.validate(data);
-        return valid;
-    } catch (error) {
-        if (error instanceof yup.ValidationError) {
-            console.log(error);
-            return error;
-        } else {
-            return false;
-        }
+  try {
+    let valid = await schema.validate(data);
+    return valid;
+  } catch (error) {
+    if (error instanceof yup.ValidationError) {
+      return error;
+    } else {
+      return false;
     }
-    
-    
+  }
 }
