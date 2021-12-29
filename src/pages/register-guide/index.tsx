6a0a8f91a5788775bb982749991684ff373d8d16
Roberta Azambuja <robertaazambuja01@gmail.com>
2021-12-29 15:52:50 -0300

    Criação do formulário

--- src/pages/register-guide/index.tsx
+++ src/pages/register-guide/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as yup from 'yup';
+import { object } from 'yup/lib/locale';
 
 export const RegisterGuide: React.FC = (): JSX.Element => {
-  return <>...</>;
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="titulo">Título</label>
+        <input
+          type="text"
+          id="titulo"
+          name="titulo"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label htmlFor="descricao">Descrição</label>
+        <textarea
+          id="descricao"
+          name="descricao"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <button type="submit">Salvar</button>
+        <button type="reset">Fechar</button>
+      </form>
+    </>
+  );
 };
 
 export default RegisterGuide;
