3d65fa17571f7ff1ac2abfca0c5b505da958738b
LizSantana <liz.santanabrito@gmail.com>
2021-12-29 14:47:39 -0300

    fix: retirando Footer import do index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Footer } from './components/Footer/index';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './styles/theme';
