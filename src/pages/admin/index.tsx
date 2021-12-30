002e6ad43b57fc2381d36f08361d30bc18399704
mgunnar <devmatheus@gmail.com>
2021-12-30 14:21:37 -0300

    Adicionando botões página admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Box, { BoxProps } from '@mui/material/Box';
+
+
+
 export interface AdminProps {}
 
 export const Admin: React.FC<AdminProps> = (): JSX.Element => {
@@ -7,15 +13,63 @@ export const Admin: React.FC<AdminProps> = (): JSX.Element => {
 
   return (
     <>
-      <button
+
+    <Box >
+
+
+   <Grid
+  container
+  direction="column"
+ justifyContent="center"
+  alignItems="center"
+   columnSpacing = {10}>
+
+   <Grid item >
+
+      <Button variant="outlined" size='large'
         onClick={() => {
           navigate('cadastrar-guia');
-        }}
+        } }
       >
         Cadastrar Guia
-      </button>
-      Teste pagina Admin
-    </>
+      </Button></Grid>
+    
+    
+      <Grid item>
+
+        <Button
+        variant="outlined" size='large'
+          onClick={() => {
+            navigate('');
+          } }
+        >
+          Cadastro de Categorias
+        </Button>
+    
+       </Grid>
+    
+      <Grid item>
+      <Button
+      variant="outlined"
+        onClick={() => {
+          navigate('');
+        }}
+      >
+        Cadastro de Conteúdo digital
+      </Button>
+  </Grid>
+
+    </Grid>
+
+         </Box>
+
+
+      
+      </>
+
+          
+      
+    
   );
 };
 
