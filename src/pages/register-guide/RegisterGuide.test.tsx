ceaa215ca8eadde5a84de21c36d697ca16c73168
arthurvergacas <arthurvdm@gmail.com>
2021-12-30 19:07:32 -0300

    Testes de submit e de input a tela de registro de guia

--- src/pages/register-guide/RegisterGuide.test.tsx
+++ src/pages/register-guide/RegisterGuide.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import RegisterGuide from './index';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import validateInput from './validator';
+
+jest.mock('./validator');
 
 describe('Página de cadastro de nova guia', () => {
   test('Deve mostrar um formulário', () => {
@@ -22,7 +26,34 @@ describe('Página de cadastro de nova guia', () => {
     expect(textArea).toBeTruthy();
   });
 
-  test('Deve executar a função handleSubmit quando o botão de submit for clicado', () => {
+  test('Deve atualizar o valor dos campos de input quando o valor destes mudar', () => {
     render(<RegisterGuide />);
+
+    const textoLabelTitulo = 'Título';
+    const textoLabelDescricao = 'Descrição';
+
+    const input = screen.getByLabelText(textoLabelTitulo);
+    const textArea = screen.getByLabelText(textoLabelDescricao);
+
+    const inputText = 'Esse é o texto presente no elemento input';
+    const textAreaText =
+      'Esse é o texto presente no elemento textarea\n Ele aceita novas linhas';
+
+    userEvent.type(input, inputText);
+    userEvent.type(textArea, textAreaText);
+
+    expect(input).toHaveValue(inputText);
+    expect(textArea).toHaveValue(textAreaText);
+  });
+
+  test('Deve validar o input quando o botão de submit for clicado', () => {
+    render(<RegisterGuide />);
+
+    const textoBotaoSubmit = 'Salvar';
+    const botaoSubmit = screen.getByText(textoBotaoSubmit);
+
+    userEvent.click(botaoSubmit);
+
+    expect(validateInput).toBeCalled();
   });
 });
