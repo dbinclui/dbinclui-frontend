2ecd72f109fae4f04f45285fcbc469bd6d9eb6ec
Roberta Azambuja <robertaazambuja01@gmail.com>
2022-01-03 18:26:37 -0300

    [feature] Estilizar página de cadastro de guia

--- src/pages/register-guide/index.tsx
+++ src/pages/register-guide/index.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import validateInput from './validator';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import './styles.css';
 
 export interface RegisterGuideProps {}
 
@@ -19,27 +23,39 @@ export const RegisterGuide: React.FC<RegisterGuideProps> = (): JSX.Element => {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="titulo">Título</label>
-        <input
-          type="text"
-          id="titulo"
-          name="titulo"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-        />
-        <label htmlFor="descricao">Descrição</label>
-        <textarea
-          id="descricao"
-          name="descricao"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-        />
-        <button type="submit">Salvar</button>
-        <button type="reset">Fechar</button>
-      </form>
-    </>
+    <main className="register-guide-wrapper" role="main">
+      <Box className="register-guide-content" component="section">
+        <header>
+          <Typography variant="h1">CADASTRO DE GUIA</Typography>
+        </header>
+        <section>
+          <Button variant="outlined">Buscar conteúdo digital</Button>
+          <form className="register-guide-form" onSubmit={handleSubmit}>
+            <label htmlFor="titulo">Título</label>
+            <input
+              type="text"
+              id="titulo"
+              name="titulo"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+            <label htmlFor="descricao">Descrição</label>
+            <textarea
+              id="descricao"
+              name="descricao"
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
+            />
+            <Button sx={{ fontWeight: 700 }} variant="outlined" type="submit">
+              Salvar
+            </Button>
+            <Button variant="outlined" type="reset">
+              Fechar
+            </Button>
+          </form>
+        </section>
+      </Box>
+    </main>
   );
 };
 
