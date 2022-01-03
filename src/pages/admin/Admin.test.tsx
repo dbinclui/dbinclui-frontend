77bf930d56cb9344c401c05703e9e6df5b39caa3
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2022-01-03 14:30:41 -0300

    feature: teste da página admin

--- src/pages/admin/Admin.test.tsx
+++ src/pages/admin/Admin.test.tsx
@@ -1,13 +1,35 @@
-import { Admin } from './index';
+import React from 'react';
+import Admin from '.';
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 
-jest.mock('../admin/index.tsx');
-
-const getAdminMock = Admin as jest.MockedFunction<typeof Admin>;
+jest.mock('react-router-dom');
 
 describe('Página do Administrador', () => {
-  beforeEach(() => {
-    getAdminMock.mockClear(); 
+  test('Deve mostrar os botões de criação de guias', () => {
+    render(<Admin />);
+    const buttonRegisterGuideText = 'Cadastrar Guia';
+    const buttonRegisterCategoryText = 'Cadastro de Categorias';
+    const buttonRegisterContentText = 'Cadastro de Conteúdo digital';
+  
+    const buttonRegisterGuide = screen.getByText(buttonRegisterGuideText);
+    const buttonRegisterCategory = screen.getByText(buttonRegisterCategoryText);
+    const buttonRegisterContent = screen.getByText(buttonRegisterContentText);
+
+    expect(buttonRegisterGuide).toBeTruthy();
+    expect(buttonRegisterCategory).toBeTruthy();
+    expect(buttonRegisterContent).toBeTruthy();
   });
-  test('Deve mostrar os botões de criação de guias', async () => {})
+  test('O botão deve redirecionar o user para outra página', () => {
+    render(<Admin />);
+    const buttonRegisterGuideText = 'Cadastrar Guia';
+    const buttonRegisterCategoryText = 'Cadastro de Categorias';
+    const buttonRegisterContentText = 'Cadastro de Conteúdo digital';
+  
+    const buttonRegisterGuide = screen.getByText(buttonRegisterGuideText);
+    const buttonRegisterCategory = screen.getByText(buttonRegisterCategoryText);
+    const buttonRegisterContent = screen.getByText(buttonRegisterContentText);
+
+    
+  });
 });
