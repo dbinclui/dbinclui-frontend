b0634940e671d76f9b2a76758bc238b852312d43
arthurvergacas <arthurvdm@gmail.com>
2022-01-07 15:21:18 -0300

    [feature] Criar o componente de notificação
    Ele é basicamente um wrapper dos componentes <Alert /> e <Snackbar /> do MUI

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Snackbar, Alert, AlertTitle } from '@mui/material';
+
+export interface NotificationProps {
+  variant: 'error' | 'success';
+  message: string;
+  title?: string;
+}
+
+export const Notification: React.FC<NotificationProps> = ({
+  variant,
+  message,
+  title,
+}): JSX.Element => {
+  const [shouldOpen, setShouldOpen] = useState(true);
+
+  const handleClose = () => setShouldOpen(false);
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      open={shouldOpen}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      sx={{ width: '35rem' }}
+    >
+      <Alert
+        variant="filled"
+        severity={variant}
+        onClose={handleClose}
+        sx={{ width: '100%' }}
+      >
+        {title ? (
+          <AlertTitle>{title}</AlertTitle>
+        ) : variant === 'error' ? (
+          <AlertTitle>Erro</AlertTitle>
+        ) : (
+          <AlertTitle>Sucesso</AlertTitle>
+        )}
+
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default Notification;
