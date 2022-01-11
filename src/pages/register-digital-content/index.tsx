612da33091426ef2dd61ac2770df5c49193ce404
Bruno Machado <brunomdr46@gmail.com>
2022-01-11 17:09:33 -0300

    Criação de rota para incluir conteúdo digital

---
+++ src/pages/register-digital-content/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface RegisterDigitalContentProps {}
+
+export const RegisterDigitalContent: React.FC<
+  RegisterDigitalContentProps> = (): JSX.Element => {
+  return <p>Teste de página de Cadastro de Conteúdo Digital</p>;
+};
+
+export default RegisterDigitalContent;
