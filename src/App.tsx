0fe3ac907f7bc206129ca0bcfaed852789992229
Dayane Mendes <day.neisca@gmail.com>
2022-01-17 19:21:31 -0300

    Atualizando path

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import VLibras from '@djpfs/react-vlibras';
-import Routers from './routes/routes';
+import Routers from '@routes/routes';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './styles/theme';
+import theme from '@styles/theme';
 import AccessibilityTools from '@components/AcessibilityTools';
 import GlobalContext from './contexts';
 
 function App() {
-  const [ themeMode, setThemeMode ] = useState('default')  
-  const toggleTheme = () => themeMode === 'default' ? 'contrast' :  'default';
+  const [themeMode, setThemeMode] = useState('default');
+  const toggleTheme = () => (themeMode === 'default' ? 'contrast' : 'default');
   return (
     <ThemeProvider theme={theme(themeMode)}>
       <GlobalContext>
         <CssBaseline />
         <VLibras />
-        <AccessibilityTools handleClickContrastButton={() => {
-          setThemeMode(toggleTheme())
-        }} />
+        <AccessibilityTools
+          handleClickContrastButton={() => {
+            setThemeMode(toggleTheme());
+          }}
+        />
         <React.StrictMode>
           <Routers />
         </React.StrictMode>
