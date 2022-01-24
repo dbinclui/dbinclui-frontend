3f4037b59d5ce881f7d71d9339cb51a7ef12f6cf
Lorena da Silva Azevedo <lorenaazevedo41@gmail.com>
2022-01-24 17:31:57 -0300

    Tentativa de renderizar linhas conforme quantidade de guias do banco

--- src/pages/categories-list/index.tsx
+++ src/pages/categories-list/index.tsx
@@ -23,11 +23,6 @@ export const CategoriesList: React.FC<
     }
   }
 
-  function titleGuide() {
-    getGuidesService();
-    return guides.title;
-  }
-
   useEffect(() => {
     getGuidesService();
   }, []);
@@ -36,9 +31,14 @@ export const CategoriesList: React.FC<
     {
       field: 'firstName',
       headerName: 'Guia',
-      width: 150,
+      width: 500,
       editable: false,
-      valueGetter: getGuidesService,
+
+      valueGetter: (getGuidesService) => {
+        return guides.map((guide) => {
+          return guide.title;
+        });
+      },
     },
     {
       field: 'lastName',
@@ -65,15 +65,9 @@ export const CategoriesList: React.FC<
   ];
 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, lastName: 'Snow', age: 35 },
+    { id: 2, lastName: 'Lannister', age: 42 },
+    { id: 3, lastName: 'Lannister', age: 45 },
   ];
 
   return (
