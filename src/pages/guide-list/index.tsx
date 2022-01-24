6a88a73a491960084a6df8e3fcb77d006aef2989
Bruno Machado <brunomdr46@gmail.com>
2022-01-24 16:02:17 -0300

    Criação da tabela dos guias
    Co-authored-by: Yasmin Castro <ysncastro@users.noreply.github.com>

--- src/pages/guide-list/index.tsx
+++ src/pages/guide-list/index.tsx
@@ -1,8 +1,78 @@
 import React from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { Typography } from '@mui/material';
 import styles from './styles';
 
 export interface GuideListProps {}
+
+const rows: GridRowsProp = [
+  {
+    id: 1,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 2,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 3,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 4,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 5,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 6,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 7,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 8,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 9,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+  {
+    id: 10,
+    col1: 'Guia de Acessibilidade',
+    col2: 'Descrição para o guia de Acessibilidade',
+    col3: 'Editar   Excluir',
+  },
+];
+
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Título', width: 250 },
+  { field: 'col2', headerName: 'Descrição', width: 560 },
+  { field: 'col3', headerName: 'Ação', width: 190 },
+];
 
 export const GuideList: React.FC<GuideListProps> = (): JSX.Element => {
   return (
@@ -10,6 +80,16 @@ export const GuideList: React.FC<GuideListProps> = (): JSX.Element => {
       <Typography variant="h2" sx={styles.listTitle}>
         LISTAGEM DE GUIAS
       </Typography>
+      <div style={{ width: '100%' }}>
+        <DataGrid
+          autoHeight
+          rows={rows}
+          columns={columns}
+          sx={styles.table}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+        />
+      </div>
     </>
   );
 };
--- src/pages/guide-list/styles.ts
+++ src/pages/guide-list/styles.ts
@@ -4,8 +4,21 @@ const listTitle: SxProps<Theme> = {
   textAlign: 'center',
 };
 
+const table: SxProps<Theme> = {
+  color: "black",
+  width: '1000px',
+  mt: '25px',
+  ml: 'auto',
+  mr: 'auto',
+  pl: '15px',
+  pr: '15px',
+  backgroundColor: 'white',
+  borderRadius: '1.5rem',
+};
+
 const styles = {
   listTitle,
+  table,
 };
 
 export default styles;
