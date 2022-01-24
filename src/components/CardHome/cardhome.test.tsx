1f0e9273c9477006d699d5ac8888590ff33f577b
nickolasL13 <nickk.lima17@gmail.com>
2022-01-24 17:33:56 -0300

    Retirando o Time To Leave de cardhome.test.tsx

--- src/components/CardHome/cardhome.test.tsx
+++ src/components/CardHome/cardhome.test.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import { screen } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect';
 import CardHome from '.';
-import { TimeToLeave } from '@mui/icons-material';
 
 jest.mock('react-router-dom', () => {
   const useHref = jest.fn();
