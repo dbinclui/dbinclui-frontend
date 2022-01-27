9539ea13c6c19949b5e707d613fe73e8b29c6dcb
fabiojcbatista <fabiojcbweb@gmail.com>
2022-01-27 10:58:15 -0300

    Ajustes na edição e exclusão

--- src/pages/list-digital-content/index.tsx
+++ src/pages/list-digital-content/index.tsx
@@ -19,30 +19,16 @@ const columns: GridColDef[] = [
     renderCell: (params) => <img src={params.value} width={60} height={50} />,
   },
   {
-    field: 'actions',
-    headerName: 'Ação',
+    field: 'edit',
+    headerName: 'Edição',
     width: 150,
-    renderCell: () => {
-      const onClick = (e: { stopPropagation: () => void }) => {
-        e.stopPropagation();
-      };
-
-      return (
-        <>
-          <Button
-            component={Link}
-            to="/admin/atualizar-categoria"
-            sx={styles.buttonTable}
-            onClick={onClick}
-          >
-            Editar
-          </Button>
-          <Button sx={styles.buttonTable} onClick={onClick}>
-            Excluir
-          </Button>
-        </>
-      );
-    },
+    renderCell: (params) => <a href={params.value}>Editar</a>
+  },
+  {
+    field: 'delete',
+    headerName: 'Exclusão',
+    width: 150,
+    renderCell: (params) => <a href={params.value}>Excluir</a>
   },
 ];
 
@@ -54,47 +40,54 @@ const rows = [
     description: 'Descrição do conteúdo digital',
     files:
       'http://2.bp.blogspot.com/-u8DXzfyQ2zo/UmVIMwaabUI/AAAAAAAA8j8/_eR_7WpYXrg/s1600/guiavidente.jpg',
-    action: 'Editar  Excluir',
+    edit: '/admin/editar-conteudo-digital?id=1',
+    delete: '/admin/excluir-conteudo-digital?id=1',
   },
   {
     id: 2,
     guide: 'Guia de Acessibilidade',
     category: 'O que é acessibilidade?',
     description: 'Descrição do conteúdo digital',
-    files: '35',
-    action: 'Editar  Excluir',
+    files: 'https://cdn.pixabay.com/photo/2017/05/20/13/10/handicap-parking-2328893_1280.jpg',
+    edit: '/admin/editar-conteudo-digital?id=2',
+    delete: '/admin/excluir-conteudo-digital?id=2',
   },
   {
     id: 3,
     guide: 'Guia de Acessibilidade',
     category: 'O que é acessibilidade?',
     description: 'Descrição do conteúdo digital',
-    files: '35',
-    action: 'Editar  Excluir',
+    files: 'https://cdn.pixabay.com/photo/2018/01/17/20/43/wheelchair-3088991_1280.jpg',
+    edit: '/admin/editar-conteudo-digital?id=3',
+    delete: '/admin/excluir-conteudo-digital?id=3',
   },
   {
     id: 4,
     guide: 'Guia do deficiente visual',
     category: 'Introdução à LIBRAS',
     description: 'Descrição do conteúdo digital',
-    files: '35',
-    action: 'Editar  Excluir',
+    files: 'http://2.bp.blogspot.com/-u8DXzfyQ2zo/UmVIMwaabUI/AAAAAAAA8j8/_eR_7WpYXrg/s1600/guiavidente.jpg',
+    edit: '/admin/editar-conteudo-digital?id=4',
+    delete: '/admin/excluir-conteudo-digital?id=4',
   },
   {
     id: 5,
     guide: 'Guia do deficiente visual',
     category: 'Números em LIBRAS',
     description: 'Descrição do conteúdo digital',
-    files: '35',
-    action: 'Editar  Excluir',
+    files: 'https://cdn.pixabay.com/photo/2017/05/20/13/10/handicap-parking-2328893_1280.jpg',
+    edit: '/admin/editar-conteudo-digital?id=5',
+    delete: '/admin/excluir-conteudo-digital?id=5',
   },
   {
     id: 6,
     guide: 'Guia do deficiente visual',
     category: 'Iniciativas inclusivas',
     description: 'Descrição do conteúdo digital',
-    files: '35',
-    action: 'Editar  Excluir',
+    files:
+      'http://2.bp.blogspot.com/-u8DXzfyQ2zo/UmVIMwaabUI/AAAAAAAA8j8/_eR_7WpYXrg/s1600/guiavidente.jpg',
+    edit: '/admin/editar-conteudo-digital?id=6',
+    delete: '/admin/excluir-conteudo-digital?id=6',
   },
 ];
 
@@ -126,6 +119,7 @@ export const ListDigitalContent: React.FC<
           variant="contained"
           type="submit"
           role="button"
+          data-testid = "new"
         >
           Novo
         </Button>
@@ -137,6 +131,7 @@ export const ListDigitalContent: React.FC<
           variant="contained"
           type="reset"
           role="button"
+          data-testid="back"
         >
           Voltar
         </Button>
