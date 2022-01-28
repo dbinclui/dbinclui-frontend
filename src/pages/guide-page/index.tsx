5a2f8eb4217f46b8b73b91d1025d968c93d0acde
nickolasL13 <nickk.lima17@gmail.com>
2022-01-28 15:59:16 -0300

    Adição da tela de loading

--- src/pages/guide-page/index.tsx
+++ src/pages/guide-page/index.tsx
@@ -9,6 +9,7 @@ import { useLocation } from 'react-router-dom';
 import { Box, Grid, Typography, Link } from '@mui/material';
 import styles from './styles';
 import AccessibilityTypography from '@components/AccessibilityTypography';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export interface GuidePageProps {}
 
@@ -17,33 +18,36 @@ export const GuidePage: React.FC<GuidePageProps> = (): JSX.Element => {
   const id = (location.state as any).id;
 
   const [guide, setGuide] = useState<GuideContent>();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getGuide() {
       try {
+        setLoading(true);
         const response = await getGuideWithCategoriesAndContent(id);
         setGuide(response.data.data);
-        // console.log(response.data.data);
       } catch (error) {
         setGuide(undefined);
+      } finally {
+        setLoading(false);
       }
     }
 
     !guide && getGuide();
-
-    //console.log(guide);
   }, [id, guide]);
 
-  return !guide ? (
-    <Box sx={styles.errorMessage}>Erro na busca do guia!</Box>
+  return loading ? (
+    <Box sx={styles.errorMessage}> <CircularProgress color="secondary"/> </Box>
+  ) : !guide ? (
+    <Box sx={styles.errorMessage}> Erro na busca do Guia! </Box>
   ) : (
     <Grid container component="main">
       {/* Indíce */}
-      
+
       <AccessibilityTypography sx={styles.indexFirst}>
         Categorias:
       </AccessibilityTypography>
-      <Grid item md={4} sx={styles.indexWrapper} >
+      <Grid item md={4} sx={styles.indexWrapper}>
         {guide?.categories.map((category, index) => {
           return (
             <Grid item md={4} sx={styles.buttonWrapper} key={category._id}>
@@ -53,7 +57,6 @@ export const GuidePage: React.FC<GuidePageProps> = (): JSX.Element => {
                 onClick={() => {
                   const scrollTo = document.getElementById(`${category._id}`);
                   scrollTo?.scrollIntoView();
-                  console.log(window.scrollY);
                 }}
               >
                 <AccessibilityTypography data-testid="IndexTitleTest">
@@ -64,32 +67,32 @@ export const GuidePage: React.FC<GuidePageProps> = (): JSX.Element => {
           );
         })}
       </Grid>
-      
+
       {/* Conteúdo */}
       <Grid item md={8} width={'100%'}>
         <Grid item md={8}>
-        <Box component="header">
-          <Typography component="h1" sx={styles.guideTitle}>
-            {guide?.title}
-          </Typography>
-          <AccessibilityTypography
-            component="h2"
-            sx={styles.guideContent}
-            id={guide?._id}
-          >
-            {guide?.content}
-          </AccessibilityTypography>
+          <Box component="header">
+            <Typography component="h1" sx={styles.guideTitle}>
+              {guide?.title}
+            </Typography>
+            <AccessibilityTypography
+              component="h2"
+              sx={styles.guideContent}
+              id={guide?._id}
+            >
+              {guide?.content}
+            </AccessibilityTypography>
 
-          {guide?.digitalContents && !!guide?.digitalContents.length && (
-            <Box sx={styles.digitalContent}>
-              <ImageCarroussel
-                contents={guide?.digitalContents}
-                height="20rem"
-                width="100%"
-              />
-            </Box>
-          )}
-        </Box>
+            {guide?.digitalContents && !!guide?.digitalContents.length && (
+              <Box sx={styles.digitalContent}>
+                <ImageCarroussel
+                  contents={guide?.digitalContents}
+                  height="20rem"
+                  width="100%"
+                />
+              </Box>
+            )}
+          </Box>
         </Grid>
 
         {guide?.categories.map((category, index) => {
