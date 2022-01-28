0915695adaac63ecd90b4ad85dc12c9e39d74d97
Roberta Azambuja <robertaazambuja01@gmail.com>
2022-01-28 15:42:23 -0300

    Alterar rotas e corrigir o teste

--- src/services/guides/index.ts
+++ src/services/guides/index.ts
@@ -17,13 +17,13 @@ export interface GuideContent {
   _id?: string;
   title: string;
   content: string;
-  categories: CategoryContent[],
-  digitalContents: CardDigitalContentResponse[],
+  categories: CategoryContent[];
+  digitalContents: CardDigitalContentResponse[];
 }
 
 export const getGuides = async () => {
   try {
-    return api.get<{ data: CardGuidesResponse[] }>(`/guides/list`);
+    return api.get<{ data: CardGuidesResponse[] }>(`/guides/`);
   } catch {
     throw new Error('Serviço não disponível');
   }
@@ -31,7 +31,7 @@ export const getGuides = async () => {
 
 export const postGuides = async (cardBody: CardBodyInterface) => {
   try {
-    return api.post('/guides/register', cardBody);
+    return api.post('/guides/', cardBody);
   } catch {
     throw new Error('Serviço não disponível');
   }
@@ -39,9 +39,10 @@ export const postGuides = async (cardBody: CardBodyInterface) => {
 
 export const getGuideWithCategoriesAndContent = async (id: string) => {
   try {
-    return api.get<{ data: GuideContent }>(`guides/getGuideWithCategoriesAndContent/${id}`);
+    return api.get<{ data: GuideContent }>(
+      `guides/getGuideWithCategoriesAndContent/${id}`,
+    );
   } catch {
     throw new Error('Serviço não disponível');
   }
 };
-
