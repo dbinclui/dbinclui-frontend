98b374d1b8e7a5bc58eda7b76406bb48432adf99
Bruno Machado <brunomdr46@gmail.com>
2022-02-04 10:38:37 -0300

    Criação da página Co-authored-by: Yasmin Castro <ysncastro@users.noreply.github.com>

---
+++ src/pages/update-digital-content/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface UpdateDigitalContentProps {}
+
+export const UpdateDigitalContent: React.FC<
+  UpdateDigitalContentProps
+> = (): JSX.Element => {
+  return <h1>Atualizar Conteúdo Digital</h1>;
+};
+
+export default UpdateDigitalContent;
