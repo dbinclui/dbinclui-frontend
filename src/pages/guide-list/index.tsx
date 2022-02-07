d4302059c789bcd5a979d547a59769bce3ddbf7a
Roberta Azambuja <robertaazambuja01@gmail.com>
2022-02-07 13:10:57 -0300

    Revert "Atualizando lista de guias"
    This reverts commit 980f1c40aad84667fa75e5d3db4216da9d0d69b2.

--- src/pages/guide-list/index.tsx
+++ src/pages/guide-list/index.tsx
@@ -37,7 +37,6 @@ export const GuideList: React.FC<
     try {
       const { data } = await deleteGuide(id);
       setSuccess(true);
-      getGuideListService();
     } catch (error: any) {
       setErrorMessage(error.response.data.message);
       setError(true);
@@ -46,7 +45,7 @@ export const GuideList: React.FC<
 
   useEffect(() => {
     getGuideListService();
-  }, [guideList]);
+  }, []);
 
   const columns: GridColDef[] = [
     {
