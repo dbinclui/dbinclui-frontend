0c944f422926cd12260bd9797f08af80258e6d05
nickolasL13 <nickk.lima17@gmail.com>
2022-02-08 09:28:07 -0300

    Testes de services/digitalContent concluídos

--- src/services/digitalContent/digitalContent.test.ts
+++ src/services/digitalContent/digitalContent.test.ts
@@ -1,7 +1,7 @@
 import {
- getCategoriesByGuide,
- getDigitalContent,
- //postDigitalContent,
+  getCategoriesByGuide,
+  getDigitalContent,
+  postDigitalContent,
 } from '@services/digitalContent';
 
 import api from '@services/api';
@@ -9,101 +9,121 @@ import api from '@services/api';
 jest.mock('@services/guides');
 jest.mock('@services/api');
 
- 
 const apiMock = api as jest.Mocked<typeof api>;
 
-
 describe('Testando o serviço "getGuides"', () => {
- beforeEach(() => {
-   apiMock.get.mockClear();
- });
-
- it(`${getDigitalContent.name}: Devolvendo conteúdo "getDigitalContent"`, async () => {
-   apiMock.get.mockResolvedValue([]);
-   const result = await getDigitalContent();
-   expect(apiMock.get).toBeCalledTimes(1);
-   expect(result).toStrictEqual([]);
- });
-
- it(`${getDigitalContent.name}: Tratamento de erro quando o serviço não estiver disponível`, async () => {
-  const errorMessage = 'Serviço não disponível';
-  const throwError = new Error(errorMessage);
-  apiMock.get.mockImplementation(() => {
-    throw throwError;
+  beforeEach(() => {
+    apiMock.get.mockClear();
   });
-  try {
-    await getDigitalContent();
-  } catch {}
-  expect(apiMock.get).toBeCalledTimes(1);
-  expect(apiMock.get).toThrow(Error);
-  expect(apiMock.get).toThrow(errorMessage);
+
+  it(`${getDigitalContent.name}: Devolvendo conteúdo "getDigitalContent"`, async () => {
+    apiMock.get.mockResolvedValue([]);
+    const result = await getDigitalContent();
+    expect(apiMock.get).toBeCalledTimes(1);
+    expect(result).toStrictEqual([]);
+  });
+
+  it(`${getDigitalContent.name}: Tratamento de erro quando o serviço não estiver disponível`, async () => {
+    const errorMessage = 'Serviço não disponível';
+    const throwError = new Error(errorMessage);
+    apiMock.get.mockImplementation(() => {
+      throw throwError;
+    });
+    try {
+      await getDigitalContent();
+    } catch {}
+    expect(apiMock.get).toBeCalledTimes(1);
+    expect(apiMock.get).toThrow(Error);
+    expect(apiMock.get).toThrow(errorMessage);
+  });
 });
-});
-/* 
+
 describe('Testando o serviço "postGuides"', () => {
-beforeEach(() => {
-  apiMock.post.mockClear();
-});
-});
-
-it(`Quando ${postDigitalContent.name} é chamado, o resultado deve retornar true
-`, async () => {
-
-  const guide:cardBody = '';
-  const category= 'category';
-  const title= 'titulo';
-  const shortDescription= 'Descrição';
-  const files= '';
-  const pathExpect = '/digital-contents/';
-  const resultExpect = true;
-  apiMock.post.mockResolvedValue(resultExpect);
-  const result = await postDigitalContent({ guide, title, shortDescription, files });
-  expect(result).toBe(resultExpect);
-  expect(apiMock.post).toBeCalledWith(pathExpect, { title, content });
-});
-
-it(`${postGuides.name}: Tratamento de erro quando o serviço não estiver disponível`, async () => {
-  const title = 'Test title';
-  const content = 'Teste content';
-  const errorMessage = 'Serviço não disponível';
-  const throwError = new Error(errorMessage);
-  apiMock.post.mockImplementation(() => {
-    throw throwError;
+  beforeEach(() => {
+    apiMock.post.mockClear();
   });
-  try {
-    await postGuides({ title, content });
-  } catch {}
-  expect(apiMock.post).toBeCalledTimes(1);
-  expect(apiMock.post).toThrow(Error);
-  expect(apiMock.post).toThrow(errorMessage);
+
+  it(`Quando ${postDigitalContent.name} é chamado, o resultado deve retornar true`, async () => {
+    const digitalContentBody = {
+      title: 'title-teste',
+      shortDescription: 'shortDescription-teste',
+      guide: '1',
+      category: '2',
+    } as { [key: string]: any };
+
+    const fileTest = new File([''], 'fileTest', { type: '/img' });
+    const formDataTest = new FormData();
+
+    Object.keys(digitalContentBody).forEach((key) => {
+      formDataTest.append(key, digitalContentBody[key]);
+    });
+    formDataTest.append('files', fileTest);
+
+    const pathExpect = '/digital-contents/';
+    const resultExpect = true;
+    apiMock.post.mockResolvedValue(resultExpect);
+    const result = await postDigitalContent(formDataTest);
+    expect(result).toBe(resultExpect);
+    expect(apiMock.post).toBeCalledWith(pathExpect, formDataTest, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+  });
+
+  it(`${postDigitalContent.name}: Tratamento de erro quando o serviço não estiver disponível`, async () => {
+    const digitalContentBody = {
+      title: 'title-teste',
+      shortDescription: 'shortDescription-teste',
+      guide: '1',
+      category: '2',
+    } as { [key: string]: any };
+
+    const fileTest = new File([''], 'fileTest', { type: '/img' });
+    const formDataTest = new FormData();
+
+    Object.keys(digitalContentBody).forEach((key) => {
+      formDataTest.append(key, digitalContentBody[key]);
+    });
+    formDataTest.append('files', fileTest);
+
+    const errorMessage = 'Serviço não disponível';
+    const throwError = new Error(errorMessage);
+    apiMock.post.mockImplementation(() => {
+      throw throwError;
+    });
+    try {
+      await postDigitalContent(formDataTest);
+    } catch {}
+    expect(apiMock.post).toBeCalledTimes(1);
+    expect(apiMock.post).toThrow(Error);
+    expect(apiMock.post).toThrow(errorMessage);
+  });
 });
-});
- */
+
 describe('Testando o serviço "getCategoriesByGuide"', () => {
-beforeEach(() => {
-  apiMock.get.mockClear();
-});
-
-it(`${getCategoriesByGuide.name}: Devolvendo conteúdo "getCategoriesByGuide"`, async () => {
-  const id = '1';
-  apiMock.get.mockResolvedValue([]);
-  const result = await getCategoriesByGuide(id);
-  expect(apiMock.get).toBeCalledTimes(1);
-  expect(result).toStrictEqual([]);
-});
-
-it(`${getCategoriesByGuide.name}: Tratamento de erro quando o serviço não estiver disponível`, async () => {
-  const id = '1';
-  const errorMessage = 'Serviço não disponível';
-  const throwError = new Error(errorMessage);
-  apiMock.get.mockImplementation(() => {
-    throw throwError;
+  beforeEach(() => {
+    apiMock.get.mockClear();
   });
-  try {
-    await getCategoriesByGuide(id);
-  } catch {}
-  expect(apiMock.get).toBeCalledTimes(1);
-  expect(apiMock.get).toThrow(Error);
-  expect(apiMock.get).toThrow(errorMessage);
-});
+
+  it(`${getCategoriesByGuide.name}: Devolvendo conteúdo "getCategoriesByGuide"`, async () => {
+    const id = '1';
+    apiMock.get.mockResolvedValue([]);
+    const result = await getCategoriesByGuide(id);
+    expect(apiMock.get).toBeCalledTimes(1);
+    expect(result).toStrictEqual([]);
+  });
+
+  it(`${getCategoriesByGuide.name}: Tratamento de erro quando o serviço não estiver disponível`, async () => {
+    const id = '1';
+    const errorMessage = 'Serviço não disponível';
+    const throwError = new Error(errorMessage);
+    apiMock.get.mockImplementation(() => {
+      throw throwError;
+    });
+    try {
+      await getCategoriesByGuide(id);
+    } catch {}
+    expect(apiMock.get).toBeCalledTimes(1);
+    expect(apiMock.get).toThrow(Error);
+    expect(apiMock.get).toThrow(errorMessage);
+  });
 });
