a8591726abdba5a75540651be1174ad578ea9552
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2022-02-10 11:25:34 -0300

    Alteração no texto

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -11,10 +11,20 @@ export const About: React.FC<AboutProps> = (): JSX.Element => {
       <main className="about">
         <Outlet />
         <AccessibilityTypography padding={'10rem 3rem'} textAlign={'center'}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
           gravida nibh, ullamcorper rhoncus nibh. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit. Vivamus nunc elit, vehicula eget dolor
-          nec, euismod dignissim libero.
+          nec, euismod dignissim libero. Quisque eget efficitur lorem. Ut at
+          rhoncus neque. Donec facilisis finibus faucibus. Fusce tincidunt elit
+          at nisl venenatis, viverra bibendum augue volutpat. Curabitur pretium
+          elit nisl, nec fringilla sem faucibus quis. Class aptent taciti
+          sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Duis a lorem non mauris congue iaculis in eu velit. Fusce
+          eget pharetra nisl. Integer ut finibus massa. Cras rutrum est nec
+          ligula sollicitudin vehicula at sed est. Donec ornare tempor
+          porttitor. Pellentesque arcu eros, interdum ut leo quis, fermentum
+          consequat purus. Aliquam erat volutpat. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit.
         </AccessibilityTypography>
       </main>
     </>
